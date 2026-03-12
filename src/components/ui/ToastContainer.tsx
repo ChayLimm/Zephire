27c0aab3f85902301d17138dabbc6c2ca118629c
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2026-03-12 13:22:46 +0700

    Fix bug

--- src/components/ui/ToastContainer.tsx
+++ src/components/ui/ToastContainer.tsx
@@ -55,7 +55,7 @@ export default function ToastContainer() {
 
   return (
     <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-2">
-      {notifications.map(n => (
+      {notifications.map((n: Notification) => (
         <Toast key={n.id} notification={n} />
       ))}
     </div>
